9a18aa8fafbd241f750494a0f2530ebd6e33a973
keinsta <accounts@keinsta.com>
2025-08-31 16:43:01 +0500

    second commit

--- app/components/helper/animation-lottie.jsx
+++ app/components/helper/animation-lottie.jsx
@@ -1,20 +1,30 @@
 "use client";
 
-import Lottie from "lottie-react";
+import { useEffect, useRef } from "react";
 
 const AnimationLottie = ({ animationPath, width }) => {
-  const defaultOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: animationPath,
-    style: {
-      width: '95%',
-    }
-  };
+  const containerRef = useRef(null);
 
-  return (
-    <Lottie {...defaultOptions} />
-  );
+  useEffect(() => {
+    let lottie;
+    import("lottie-web").then((module) => {
+      lottie = module.default;
+      lottie.loadAnimation({
+        container: containerRef.current,
+        renderer: "svg",
+        loop: true,
+        autoplay: true,
+        animationData: animationPath,
+      });
+    });
+    return () => {
+      if (lottie && containerRef.current) {
+        lottie.destroy();
+      }
+    };
+  }, [animationPath]);
+
+  return <div ref={containerRef} style={{ width: width || "95%" }} />;
 };
 
 export default AnimationLottie;
