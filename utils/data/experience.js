16f508d91015fa43dc2675da0d1ad832d266bd3e
keinsta <accounts@keinsta.com>
2025-08-31 15:21:49 +0500

    first commit

---
+++ utils/data/experience.js
@@ -0,0 +1,26 @@
+export const experiences = [
+  {
+    id: 1,
+    title: "MERN Stack Developer",
+    company: "Soft Access, Johar Town, Lahore",
+    duration: "(Oct 2022 - Present)",
+  },
+  {
+    id: 2,
+    title: "NodeJS Developer",
+    company: "Valid Sole, Remote Job",
+    duration: "(July 2021 - Aug 2022)",
+  },
+  {
+    id: 3,
+    title: "Web Developer (Internship)",
+    company: "Eskatchers, Johar Town, Lahore",
+    duration: "(Feb 2021 - May 2021)",
+  },
+  {
+    id: 4,
+    title: "Upwork BDE (Internship)",
+    company: "GeniousDev, Iqbal Town, Lahore",
+    duration: "(June 2022 - Sep 2022)",
+  },
+];
---
+++ utils/data/projects-data.js
@@ -0,0 +1,69 @@
+export const projectsData = [
+  {
+    id: 1,
+    name: "MedEx Global",
+    description:
+      "I developed a web application for the medical field that allows patients to find doctors based on their requirements and budget. The platform enables patients to contact doctors directly and includes a video streaming feature for online consultations. With a user-friendly interface and seamless functionality, this project ensures a smooth experience for both patients and doctors. Built using modern technologies, it provides a scalable solution tailored to the healthcare industry.",
+    tools: [
+      "Express",
+      "MongoDB",
+      "OpenAI API",
+      "AWS Cloud9",
+      "Vercel",
+      "Material UI",
+      "Node Mailer",
+      "Tailwind CSS",
+    ],
+    role: "Backend Developer",
+    code: "",
+    demo: "",
+  },
+  {
+    id: 2,
+    name: "Deep AI Platform",
+    description:
+      "I collaborated with a development team to build a powerful AI platform that offers a wide range of tools — including AI Chat, Image & Video Generation, Voice Chat, Music, Photo Editing, and Math Solving capabilities. I led the development of two core modules: 1) Built using advanced APIs like Stability AI, enabling users to generate stunning images based on text prompts. 2) Engineered a smart math solver capable of understanding and solving complex equations using LaTeX formatting and step-by-step output.",
+    tools: [
+      "NextJS",
+      "NodeJS",
+      "Tailwind CSS",
+      "Rest API",
+      "Generative AI",
+      "ChatGPT API Integration",
+      "Web Forms",
+    ],
+    role: "Full Stack Developer",
+    code: "",
+    demo: "",
+  },
+  {
+    id: 3,
+    name: "Apceco AE",
+    description:
+      "Built a modern, responsive platform for a Dubai-based company offering poultry services, heavy crane construction, and industry-specific e-commerce. 𝐇𝐢𝐠𝐡𝐥𝐢𝐠𝐡𝐭𝐬: Admin dashboard, category-based content, mobile-optimized, dynamic CMS.",
+    tools: [
+      "React",
+      "Bootstrap",
+      "SCSS",
+      "Stripe",
+      "Express",
+      "TypeScript",
+      "MongoDB",
+      "Cronjob",
+      "JWT",
+    ],
+    code: "",
+    role: "Full Stack Engineer",
+    demo: "",
+  },
+  {
+    id: 4,
+    name: "Design Vision",
+    description:
+      "My team and I developed Design Vision. Design Vision is an e-commerce platform dedicated to transforming spaces with modern interior design products. Whether you're looking to revamp a single room or complete a full home makeover, Design Vision offers a curated selection of high-quality furniture, décor, and accessories. The platform provides a seamless shopping experience, allowing users to discover and purchase stylish products that bring their design visions to life.",
+    tools: ["NextJS", "Material UI", "Redux", "Sun Editor", "Calendar"],
+    code: "",
+    demo: "",
+    role: "Full Stack Developer",
+  },
+];
